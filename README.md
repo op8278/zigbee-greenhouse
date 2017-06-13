78e6efa402bfbc649fa47851e9dc82d816be60b2
op8278 <406386343@qq.com>
2017-06-13 22:13:41 +0800

    增加测试sql文件

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # zigbee-greenhouse
-zigbee课程设计 大棚监控系统 Java服务端 
+zigbee课程设计 大棚监控系统 Java服务端(为上位机提供接口API) 
+
+将 \src\main\resources\applicationContext-datasource.xml 里的mysql参数替换为自身的
+
+db.url=jdbc:mysql://127.0.0.1:3306/zigbee?characterEncoding=utf-8 //jdbc连接url
+db.username=root  //mysql登录帐号
+db.password=root  //mysql登录密码
+
+将 \src\main\resources\logback.xml 里的File/fileNamePattern字段参数替换本地日志保存位置
+<File>${CATALINA_HOME}/logs/zigbee_error.log</File>
+<!--<File>f:/zigbeelog/error.log</File>-->
+<fileNamePattern>${CATALINA_HOME}/logs/zigbee_error.log.%d{yyyy-MM-dd}.gz</fileNamePattern>
--- zigbee.sql
+++ zigbee.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50709
 File Encoding         : 65001
 
-Date: 2017-06-12 22:05:59
+Date: 2017-06-13 22:03:45
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -26,7 +26,7 @@ CREATE TABLE `zigbee_receive_data` (
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   `update_time` datetime DEFAULT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of zigbee_receive_data
@@ -34,3 +34,12 @@ CREATE TABLE `zigbee_receive_data` (
 INSERT INTO `zigbee_receive_data` VALUES ('1', '30', '15', '2017-04-13 21:27:06', '2017-04-13 21:27:06');
 INSERT INTO `zigbee_receive_data` VALUES ('2', '25', '10', '2017-04-13 21:27:06', '2017-04-13 21:27:06');
 INSERT INTO `zigbee_receive_data` VALUES ('3', '111', '222', '2017-06-12 22:03:02', '2017-06-12 22:03:02');
+INSERT INTO `zigbee_receive_data` VALUES ('4', '222', '33', '2017-06-12 22:16:46', '2017-06-12 22:16:46');
+INSERT INTO `zigbee_receive_data` VALUES ('5', '33', '33', '2017-06-13 16:11:15', '2017-06-13 16:11:15');
+INSERT INTO `zigbee_receive_data` VALUES ('6', '22', '34', '2017-06-13 16:11:57', '2017-06-13 16:11:57');
+INSERT INTO `zigbee_receive_data` VALUES ('7', '22', '3', '2017-06-13 16:14:38', '2017-06-13 16:14:38');
+INSERT INTO `zigbee_receive_data` VALUES ('8', '1', '20', '2017-06-13 17:55:30', '2017-06-13 17:55:30');
+INSERT INTO `zigbee_receive_data` VALUES ('9', '30', '20', '2017-06-13 18:02:31', '2017-06-13 18:02:31');
+INSERT INTO `zigbee_receive_data` VALUES ('10', '13', '33', '2017-06-13 19:11:54', '2017-06-13 19:11:54');
+INSERT INTO `zigbee_receive_data` VALUES ('11', '14', '14', '2017-06-14 21:09:30', '2017-06-14 21:09:38');
+INSERT INTO `zigbee_receive_data` VALUES ('12', '17', '17', '2017-07-13 21:09:54', '2017-07-13 21:10:02');
